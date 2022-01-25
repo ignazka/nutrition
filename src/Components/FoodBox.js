fad45e3ac4b1dd920589d32e26c075265beedac2
Philipp Kastl <picastle@posteo.de>
2022-01-25 16:30:39 +0100

    cra clone with styling

---
+++ src/Components/FoodBox.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "bulma/css/bulma.css";
+
+const FoodBox = ({ name, image, calories, quantity, onClick }) => {
+  const [state, setState] = React.useState(quantity);
+
+  const handleChange = ({ target }) => setState(target.value);
+  const handleClick = () => {
+    console.log("click");
+    onClick({ name, calories, image, quantity: state });
+  };
+
+  return (
+    <div key={name} className="FoodBox">
+      <article className="media">
+        <div className="media-left">
+          <figure className="image is-64x64">
+            <img src={image} alt={name} />
+          </figure>
+        </div>
+        <div className="media-content">
+          <div className="content">
+            <p>
+              <strong>{name}</strong> <br />
+              <p>
+                <small>{calories} cal</small>
+              </p>
+            </p>
+          </div>
+        </div>
+        <div className="media-right">
+          <div className="field has-addons">
+            <div className="control">
+              <input
+                className="input"
+                type="number"
+                value={state}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="control">
+              <button onClick={handleClick} className="button is-info">
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default FoodBox;
---
+++ src/Components/FoodForm.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+const FoodForm = ({ onClick}) => {
+  const initialState = { name: '', calories: 0, image: '', quantity: 0 };
+  const [state, setState] = React.useState(initialState);
+  const handleChange = ({ target }) => {
+    setState({ ...state, [target.name]: target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newFood = { ...state };
+
+    if (state.calories) {
+      onClick(newFood);
+    }
+    setState(initialState);
+  };
+
+  // const handleShow = (event) => {
+  //   event.preventDefault();
+  //   console.log(event.target)
+  //   onClick({state: event.target.value})
+  // }
+  return (
+    <form className="form FoodForm" onSubmit={handleSubmit}>
+      <label className="label" htmlFor="name">
+        Name
+      </label>
+      <input
+        className="input"
+        type="text"
+        name="name"
+        value={state.name}
+        onChange={handleChange}
+      />
+
+      <label className="label" htmlFor="image">
+        Image URL
+      </label>
+      <input
+        className="input"
+        type="text"
+        name="image"
+        value={state.image}
+        onChange={handleChange}
+      />
+      <label className="label" htmlFor="calories">
+        Calories
+      </label>
+      <input
+        className="input"
+        type="number"
+        name="calories"
+        value={state.calories}
+        onChange={handleChange}
+      />
+      <button className="button is-link" type="submit">
+        ADD
+      </button>
+      {/* <button className='button is-link is-light' value='false' type='submit' onClick={handleShow}>
+        Cancel
+      </button> */}
+    </form>
+  );
+};
+export default FoodForm;
